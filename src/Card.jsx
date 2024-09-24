e68daa16179f0af6539d9ed28dd6b264a25f4582
namtran0507 <mrvulghe@gmail.com>
2024-09-24 14:21:45 +0700

    Your commit message

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,11 @@ import profilePic from './assets/111.jpg'
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt = "profile picture" ></img>
-            <h2> Nam  </h2>
+            <img src={profilePic} alt = "profile picture"
+            style={{ width: '200px', height: 'auto' }}
+            
+            ></img>
+            <h2> hello  </h2>
 
         </div>
     );
